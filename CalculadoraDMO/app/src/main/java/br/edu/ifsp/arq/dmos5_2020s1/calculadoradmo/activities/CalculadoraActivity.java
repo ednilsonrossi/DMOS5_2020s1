d2c8001038bd62e8823d6ea5f7ac1cab851a9794
Ednilson Rossi <ednilsonrossi@gmail.com>
2020-03-13 10:44:29 -0300

    CalculadoraDMO: interface da versao 1

---
+++ CalculadoraDMO/app/src/main/java/br/edu/ifsp/arq/dmos5_2020s1/calculadoradmo/activities/CalculadoraActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.arq.dmos5_2020s1.calculadoradmo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifsp.arq.dmos5_2020s1.calculadoradmo.R;
+
+public class CalculadoraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+    }
+}
