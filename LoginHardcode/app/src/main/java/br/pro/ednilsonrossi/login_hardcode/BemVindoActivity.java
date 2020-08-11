b8f3e045821685dba5df2f95d6140136298f8518
Ednilson Rossi <ednilsonrossi@gmail.com>
2020-08-11 17:20:28 -0300

    LoginHardcode: projeto completo

---
+++ LoginHardcode/app/src/main/java/br/pro/ednilsonrossi/login_hardcode/BemVindoActivity.java
@@ -0,0 +1,43 @@
+package br.pro.ednilsonrossi.login_hardcode;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class BemVindoActivity extends AppCompatActivity {
+
+    private TextView mensagemTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bem_vindo);
+
+        //Habilita o botão de navegação
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mensagemTextView = findViewById(R.id.text_mensagem);
+
+        Bundle parametros = getIntent().getExtras();
+        String usuario = parametros.getString(LoginActivity.USUARIO);
+        String senha = parametros.getString(LoginActivity.SENHA);
+
+        if(usuario.equals("112744") && senha.equals("447211")){
+            mensagemTextView.setText("Bem vindo " + usuario);
+        }else{
+            mensagemTextView.setText("Erro no login");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
