f561cd6923dbe7541d657e8e2e62f8c639dfa24d
Ednilson Rossi <ednilsonrossi@gmail.com>
2020-08-11 21:24:16 -0300

    LoginLocal: Salva preferencia de login

--- LoginLocal/app/src/main/java/br/pro/ednilsonrossi/loginlocal/MainActivity.java
+++ LoginLocal/app/src/main/java/br/pro/ednilsonrossi/loginlocal/MainActivity.java
@@ -3,22 +3,28 @@ package br.pro.ednilsonrossi.loginlocal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = "LOGIN_LOCAL";
 
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     private EditText usuarioEditText;
     private EditText senhaEditText;
     private Button logarButton;
+    private CheckBox lembrarCheckBox;
 
+    private String usuario, senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +34,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         usuarioEditText = findViewById(R.id.edittext_usuario);
         senhaEditText = findViewById(R.id.edittext_senha);
-
         logarButton = findViewById(R.id.button_logar);
+        lembrarCheckBox = findViewById(R.id.checkbox_lembrar);
 
         logarButton.setOnClickListener(this);
 
+        mSharedPreferences = this.getPreferences(MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
     }
+
+
 
     @Override
     protected void onStart() {
@@ -50,6 +60,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         Log.i(TAG, "Classe: " + getClass().getSimpleName() +  "| Método : onResume()");
+        verificarPreferencias();
         super.onResume();
     }
 
@@ -74,22 +85,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if(view == logarButton){
-            String usuario, senha;
+
             usuario = usuarioEditText.getText().toString();
             senha = senhaEditText.getText().toString();
-
             if(usuario.isEmpty() || senha.isEmpty()){
                 Toast.makeText(this, R.string.erro_entrada_msg, Toast.LENGTH_SHORT).show();
+                return;
             }
 
-            Intent in = new Intent(this, BemVindoActivity.class);
-            Bundle args = new Bundle();
-            args.putString(getString(R.string.key_usuario), usuario);
-            args.putString(getString(R.string.key_senha), senha);
-            in.putExtras(args);
-            startActivity(in);
+            salvaPreferencias();
+            abrirBoasVindas();
+
+
             return;
         }
 
+    }
+
+    private void abrirBoasVindas(){
+        Intent in = new Intent(this, BemVindoActivity.class);
+        Bundle args = new Bundle();
+        args.putString(getString(R.string.key_usuario), usuario);
+        args.putString(getString(R.string.key_senha), senha);
+        in.putExtras(args);
+        startActivity(in);
+    }
+
+    private void salvaPreferencias(){
+
+        if(lembrarCheckBox.isChecked()){
+            Log.i(TAG, "salvando");
+            mEditor.putString(getString(R.string.key_usuario), usuario);
+            mEditor.commit();
+
+            mEditor.putString(getString(R.string.key_senha), senha);
+            mEditor.commit();
+
+            mEditor.putBoolean(getString(R.string.key_lembrar), true);
+            mEditor.commit();
+        }else{
+            Log.i(TAG, "não salvando");
+            mEditor.putString(getString(R.string.key_usuario), "");
+            mEditor.commit();
+
+            mEditor.putString(getString(R.string.key_senha), "");
+            mEditor.commit();
+
+            mEditor.putBoolean(getString(R.string.key_lembrar), false);
+            mEditor.commit();
+        }
+
+    }
+
+    private void verificarPreferencias() {
+        usuario = mSharedPreferences.getString(getString(R.string.key_usuario), "");
+        senha = mSharedPreferences.getString(getString(R.string.key_senha), "");
+        boolean lembrar = mSharedPreferences.getBoolean(getString(R.string.key_lembrar), false);
+
+        if(lembrar){
+            usuarioEditText.setText(usuario);
+            senhaEditText.setText(senha);
+            lembrarCheckBox.setChecked(true);
+        }
     }
 }
